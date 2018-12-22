17fd44f82a2aa3147568e29c8711ba0b0c195038
Lok Yiu <lukeyeung@m800.com>
2018-12-22 12:09:41 +0800

    Add one more partner

--- src/constants/partnerList.js
+++ src/constants/partnerList.js
@@ -22,5 +22,18 @@ export default [
     otherInfo: {
       tel: '2573 2380',
     },
-  },
+	},
+	{
+		name: '伍醫師',
+		specialties: [],
+		address: {
+			lat: 22.286310,
+			lng: 114.146930,
+			region: 'central',
+			fullAddress: '上環皇后大道西77-91號荷李活中心7樓A室',
+		},
+		otherInfo: {
+			tel: '28580556（敬請預約',
+		}
+	}
 ];
