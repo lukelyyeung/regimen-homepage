bf143c36fcf6dec7d264753d780a6873f6105a06
Lok Yiu <lukeyeung@m800.com>
2019-03-09 15:41:33 +0800

    Fix the blog post edit error
    -  .toJS() should be called for string list

--- src/cms/preview-templates/BlogPostPreview.jsx
+++ src/cms/preview-templates/BlogPostPreview.jsx
@@ -3,16 +3,15 @@ import PropTypes from 'prop-types';
 import { BlogPostTemplate } from '../../templates/blog-post';
 
 const BlogPostPreview = ({ entry, widgetFor }) => {
-  const entryTags = entry.getIn(['data', 'tags']);
+  const templeProps = {
+    content: widgetFor('body'),
+    description: entry.getIn(['data', 'description']),
+    tags: entry.getIn(['data', 'tags']),
+    title: entry.getIn(['data', 'title']),
+  };
 
-  const tags = entryTags ? entryTags.toJS() : [];
   return (
-    <BlogPostTemplate
-      content={widgetFor('body')}
-      description={entry.getIn(['data', 'description'])}
-      tags={tags}
-      title={entry.getIn(['data', 'title'])}
-    />
+    <BlogPostTemplate {...templeProps} />
   );
 };
 
